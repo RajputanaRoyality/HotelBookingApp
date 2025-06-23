0a770f0ea05215a9d5c25398286a49067a7d5644
RajputanaRoyality <vaibhavrksingh6207@gmail.com>
2025-06-23 21:09:54 +0530

    last last

--- Server/server.js
+++ Server/server.js
@@ -4,20 +4,17 @@ import 'dotenv/config';
 import connectDB from './configs/db.js';
 import { clerkMiddleware } from '@clerk/express'
 import clerkWebhooks from './controller/clerkWebhoooks.js';
-// import {serve} from "inngest/express"
-// import {inngest,functions} from "../backend/inngest/index.js"
 
-const app = express();
+const app = express()
 app.use(cors())
+app.use(express.json())
+app.use(clerkMiddleware())
 
 const port = process.env.PORT || 3000;
 
 await connectDB();
 
-app.use(express.json())
-app.use(clerkMiddleware())
-
-app.use("api/clerk",clerkWebhooks);
+app.use("/api/clerk",clerkWebhooks);
 
 //API Routes
 app.get('/',(req,res)=>res.send('Server is Live!'))
