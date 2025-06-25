114091629963bd70702020757a7a0767ac118d28
RajputanaRoyality <vaibhavrksingh6207@gmail.com>
2025-06-25 12:28:57 +0530

    db2

--- Server/configs/db.js
+++ Server/configs/db.js
@@ -1,15 +1,12 @@
 import mongoose from "mongoose";
 
-const connectDB=async()=>{
+const connectDB =  async () => {
     try{
-        const conn=await mongoose.connect(`${process.env.MONGO_URI}/hotel-booking`)
-        console.log(`MongoDB Connected:${conn.connection.host}`)
+        mongoose.connection.on('connected', ()=> console.log('Database connected'));
+        await mongoose.connect(`${process.env.MONGO_URI}/vaiso-hotel`)
+    }catch(error){
+        console.log(error.message);
     }
-    catch(error){
-        console.log(`Error:${error.message}`)
-        process.exit(1)
-    }
-
 }
 
 export default connectDB;
